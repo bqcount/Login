fd5dbeb7f1a365128303a08003f0f7a78066c4f8
bqcount <bqcount@gmail.com>
2024-02-17 21:28:16 +0000

    add inputs ans styles

---
+++ index.js
@@ -0,0 +1,10 @@
+const register = document.getElementById('register-button')
+const login = document.getElementById('register-button')
+
+login.addEventListener('click',()=>{
+
+})
+
+register.addEventListener('click',()=>{
+    
+})
