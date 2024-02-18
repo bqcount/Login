f71080c504df7f00157b5adbd0bcb98db1b18f41
bqcount <bqcount@gmail.com>
2024-02-18 14:06:34 +0000

    box2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const register = document.getElementById('register-button')
-const login = document.getElementById('register-button')
+const login = document.getElementById('login-button')
+const registerLeft = document.getElementById('register-button')
+const loginRight = document.getElementById('register-button')
+
 
 login.addEventListener('click',()=>{
-
+    
+ login.classList.add('');
+      
 })
 
 register.addEventListener('click',()=>{
-    
+    register.classList.add('');
 })
