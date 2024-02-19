d4e092ac5d86ef822f2fd46687bf04a9871945dc
Daniela Romero Garcia <bqcount@gmail.com>
2024-02-19 15:37:13 +0000

    animation one done

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const register = document.getElementById('register-button')
 const login = document.getElementById('login-button')
 const registerRight = document.getElementById('register-button-rigth')
 const loginLeft = document.getElementById('login-button-left')
+const loginBoxLeft = document.getElementById('login-box-left')
+const registerBoxRight = document.getElementById('register-box-right')
+const box = document.getElementById('box')
+const box2 = document.getElementById('box2')
+const positionLoginBoxLeft = loginBoxLeft.style.left
 
 
 loginLeft.addEventListener('click', () => {
-    loginLeft.style.visibility = 'hidden';
+    loginLeft.classList.add('showFalse');
+    loginBoxLeft.classList.add('class','moveRight');
+    const idTime =setTimeout(()=>{
+        box2.classList.add('showFalse')
+        box.classList.add('showTrue')
+    },300)
+    
 });
 
-registerLeft.addEventListener('click',()=>{
 
+
+register.addEventListener('click',()=>{
+  register.classList.add('showFalse');
+  registerBoxRight.classList.add('class','moveLeft')
+  setTimeout(()=>{
+      box.classList.add('showFalse')
+      box2.classList.add('showTrue')
+},300)
       
-})
+});
 
