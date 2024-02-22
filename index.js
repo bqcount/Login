ac06c19c9d662004558db612e08f2a4ce13a2e53
bqcount <bqcount@gmail.com>
2024-02-22 19:51:12 +0000

    parte visual pmv terminada

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const register = document.getElementById('register-button')
-const registerRight = document.getElementById('register-button-rigth')
 const loginLeft = document.getElementById('login-button-left')
-const loginBoxLeft = document.getElementById('login-box-left')
-const registerBoxRight = document.getElementById('register-box-right')
 const imageBox = document.getElementById('image-box')
 const box = document.getElementById('box')
 
 
 
-/*loginLeft.addEventListener('click', () => {
+loginLeft.addEventListener('click', () => {
     loginLeft.classList.add('showFalse');
-    loginBoxLeft.classList.add('class','moveRight');
-    const idTime =setTimeout(()=>{
-        box2.classList.add('showFalse')
-        box.classList.add('showTrue')
-    },300)
-    
+    imageBox.classList.remove('class', 'moveLeft');
+    imageBox.classList.add('class', 'moveRight');
+    setTimeout(() => {
+        register.classList.remove('showFalse')
+
+    }, 300)
+
 });
-*/
 
 
-register.addEventListener('click',()=>{
-  register.classList.add('showFalse');
-  imageBox.classList.add('class','moveLeft')
-  
-  setTimeout(()=>{
-      loginLeft.classList.remove('showFalse')
-      
-},300)
-      
+
+register.addEventListener('click', () => {
+    register.classList.add('showFalse');
+    imageBox.classList.remove('class', 'moveRight');
+    imageBox.classList.add('class', 'moveLeft')
+
+    setTimeout(() => {
+        loginLeft.classList.remove('showFalse')
+
+    }, 300)
+
 });
 
